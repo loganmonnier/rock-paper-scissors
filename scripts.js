824f9701587910a0779c9f8b3ae249b8a7a114e6
Logan Monnier <lfmonnier@gmail.com>
2023-11-01 14:02:59 -0400

    add score-keeping ability to game(), finished project

--- scripts.js
+++ scripts.js
@@ -37,14 +37,62 @@ function playRound() {
 }
 
 function game(playerSelection, computerChoice) {
-    console.log(playRound(playerSelection,computerChoice));
-    console.log(playRound(playerSelection,computerChoice));
-    console.log(playRound(playerSelection,computerChoice));
-    console.log(playRound(playerSelection,computerChoice));
-    console.log(playRound(playerSelection,computerChoice));
+    let wins = 0;
+    let losses = 0;
+
+    let result1 = playRound(playerSelection,computerChoice);
+    console.log(result1);
+      if (result1.includes("win")){
+      wins++;
+    } else if (result1.includes("lose")) {
+      losses++;
+    };
+
+    let result2 = playRound(playerSelection,computerChoice);
+    console.log(result2);
+    if (result2.includes("win")){
+      wins++;
+    } else if (result2.includes("lose")) {
+      losses++;
+    };
+
+    let result3 = playRound(playerSelection,computerChoice);
+    console.log(result3);
+    if (result3.includes("win")){
+      wins++;
+    } else if (result3.includes("lose")) {
+      losses++;
+    };
+
+    let result4 = playRound(playerSelection,computerChoice);
+    console.log(result4);
+    if (result4.includes("win")){
+      wins++;
+    } else if (result4.includes("lose")) {
+      losses++;
+    };
+
+    let result5 = playRound(playerSelection,computerChoice);
+    console.log(result5);
+    if (result5.includes("win")){
+      wins++;
+    } else if (result5.includes("lose")) {
+      losses++;
+    };
+    
+    function determineWinner() {
+      if (wins >= 3){
+      return "You win! You won a majority of the rounds played.";
+    } else if (losses >= 3){
+      return "You lose! You lost a majority of the rounds played."
+    }
+    } 
+
+    return determineWinner()
+    
 }
 
-console.log(game());
 
+console.log(game())
 
 
